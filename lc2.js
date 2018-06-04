d83091f56d4274383c6612a2a5ebac7dd020e92b
William Kimball <wkkimball043@gmail.com>
2018-06-04 09:17:13 -0500

    added some explanitory text to the else statement console.log

--- lc2.js
+++ lc2.js
@@ -3,5 +3,5 @@ const lastName = "kimball"
 if (lastName.length >= 7) {
     console.log("I can't pronounce this last name")
 } else {
-    console.log(lastName)
+    console.log(`Last name: ${lastName}`)
 }
